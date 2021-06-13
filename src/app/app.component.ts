ae291f2942a50af4bd55385b0a6695ed185e58a8
Tomar <shalu.tomar@siemens.com>
2021-06-14 01:04:22 +0530

    repo search demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,123 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TypeHeadDemo';
+  repoSearch = "";
+  repoData = new Array;
+  showData : boolean = false;
+ 
+  sum = 5;
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+  direction = "";
+  completeData = new Array;
+  totalLength : number = 0;
+  maxLimit : boolean = false;
+  nameData = new Array;
+  public staticList = new Array;
+  showEmpty : boolean = false;
+  public str = "";
+  public finalArray = new Array;
+  
+ 
+
+  // public url = "https://api.github.com/search/repositories?callback=JSON_CALLBACK&q="+this.repoSearch;
+  // public params = {
+  //   // client: "youtube"
+  // };
+  // public query = "";
+
+  constructor(private http : HttpClient) {
+    //this.appendItems(0, this.sum);
+  }
+  // handleResultSelected(result:any) {
+  //   console.log(result.data);
+  //   this.query = result.data.items.full_name;
+  // }
+
+  
+  onRepoNameSubmit(value:any){
+    
+    //Reset all the values here
+    this.repoData = [];
+    this.completeData = [];
+    this.totalLength = 0;
+    this.sum = 5;
+    this.maxLimit = false;
+
+    var repoToSearch = value;
+    this.getRepoList(value).subscribe((data:any)=>{
+      this.totalLength = data.items.length
+      this.showData = true;
+      this.completeData = data.items;
+
+      for(var i =0; i<this.sum; i++){
+        if(this.completeData[i] == undefined){
+          break;
+        }
+        else{
+          this.repoData.push(this.completeData[i]);
+        }
+        
+      }
+      },(error:any)=>{
+        this.showEmpty = true;
+        console.log(error);
+     
+      }
+    );
+    
+  }
+
+
+  getRepoList(value:string):Observable<any>{
+    const endpoint = "https://api.github.com/search/repositories?q="+value;
+    return this.http.get(endpoint);
+  }
+
+  onScroll() {
+    var next : number = 0;
+    if(this.sum == this.totalLength || this.sum > this.totalLength){
+      //show the max reached limit message
+      this.maxLimit = true;
+    }
+    else{
+      //append the next data to the UI element
+      next = this.sum +5;
+      for(var i = this.sum ; i< next; i++){
+        if(this.completeData[i] == undefined){
+          break;
+        }
+        else{
+          this.repoData.push(this.completeData[i]);
+          this.sum +=1;
+        }
+        
+      }
+    }
+    
+  }
+
+  onKeypressEvent(event:any){
+    this.str="";
+    this.finalArray = [];
+    let myContainer = document.getElementById('browsers') as HTMLInputElement;
+    this.getRepoList(event.target.value).subscribe((data:any)=>{
+      this.nameData = data.items;
+      this.showEmpty = false;
+      },(error:any)=>{
+        this.showEmpty = true;
+      }
+    );
+   
+    for(var i=0; i< this.nameData.length;i++){
+      this.finalArray.push(this.nameData[i].full_name);
+    }
+    for (var i=0; i < this.finalArray.length;++i){
+      this.str += '<option value="'+this.finalArray[i]+'" />'; 
+    }
+
+    myContainer.innerHTML = this.str;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NgxTypeaheadModule } from "ngx-typeahead";
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+//import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+declare const chance:any;
 
 @NgModule({
   declarations: [
@@ -10,6 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgxTypeaheadModule,
+    InfiniteScrollModule,
     AppRoutingModule
   ],
   providers: [],
